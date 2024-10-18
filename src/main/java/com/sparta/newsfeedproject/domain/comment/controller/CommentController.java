510681124b4d83b4f550c09030e26115a5c20597
Minwoo <alsdn2606@gmail.com>
2024-10-18 16:26:15 +0900

    chore: Auth 필터 및 댓글 컨트롤러 클래스 구조 추가
    - Auth 필터 패키징 및 클래스 기본 구조 추가
    - 댓글 컨트롤러 클래스 생성 및 패키지 설정
    - 기능 구현 없이 기본적인 클래스와 패키지 구조만 설정

---
+++ src/main/java/com/sparta/newsfeedproject/domain/comment/controller/CommentController.java
@@ -0,0 +1,4 @@
+package com.sparta.newsfeedproject.domain.comment.controller;
+
+public class CommentController {
+}
---
+++ src/main/java/com/sparta/newsfeedproject/filter/AuthFilter.java
@@ -0,0 +1,4 @@
+package com.sparta.newsfeedproject.filter;
+
+public class AuthFilter {
+}

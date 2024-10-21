1b00bce1f46d659cb5d7eb085e4f470b6c6d6646
Minwoo <alsdn2606@gmail.com>
2024-10-21 11:33:09 +0900

    feat: ERD에 맞춰 엔티티 생성 및 Timestamped 엔티티 상속 구현
    - ERD에 기반하여 주요 엔티티 클래스 생성
    - Timestamped 엔티티 추가 및 생성일과 수정일을 관리하도록 상속 구현
    - 엔티티 클래스에서 상속된 필드를 통해 일관된 시간 관리 기능 제공

---
+++ src/main/java/com/sparta/newsfeedproject/domain/comment/entity/Comment.java
@@ -0,0 +1,33 @@
+package com.sparta.newsfeedproject.domain.comment.entity;
+
+import com.sparta.newsfeedproject.domain.common.TimeStamped;
+import com.sparta.newsfeedproject.domain.member.entity.Member;
+import com.sparta.newsfeedproject.domain.post.entity.Post;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Comment extends TimeStamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    // 작성자와 댓글 = 다 대 일
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id",nullable = false)
+    private Member member;
+
+    // 게시글과 댓글 = 다 대 일
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id",nullable = false)
+    private Post post;
+
+}

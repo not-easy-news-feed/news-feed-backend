0a62056741c28c12455a32b76424c428cca0d315
coldrice99 <coldrice99@gmail.com>
2024-10-22 14:49:41 +0900

    feat: 게시물 수정 기능 구현
    - 게시물 수정 API 추가 (PostService 및 PostController 작성)
    - 작성자 검증 로직 추가로 권한 없는 사용자의 수정 방지
    - PostRequestDto, PostResponseDto를 사용하여 요청 데이터와 응답 데이터 처리
    - 서비스 계층에서 게시물 수정 후 업데이트된 데이터 반환

--- src/main/java/com/sparta/newsfeedproject/domain/post/repository/PostRepository.java
+++ src/main/java/com/sparta/newsfeedproject/domain/post/repository/PostRepository.java
@@ -4,8 +4,4 @@ import com.sparta.newsfeedproject.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-
-    default Post findPostBytId(Long id){
-        return findById(id).orElseThrow(() -> new IllegalArgumentException("Post not found with id: " + id));
-    }
 }

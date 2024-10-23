d3f0bd2ee0e92d728fc88dd4d7a66442ec8916bc
coldrice99 <coldrice99@gmail.com>
2024-10-23 18:21:28 +0900

    refactor : 게시물 조회 로직 수정
    Refs: #25

--- src/main/java/com/sparta/newsfeedproject/domain/post/service/PostService.java
+++ src/main/java/com/sparta/newsfeedproject/domain/post/service/PostService.java
@@ -1,6 +1,5 @@
 package com.sparta.newsfeedproject.domain.post.service;
 
-import com.sparta.newsfeedproject.domain.comment.dto.CommentResponseDto;
 import com.sparta.newsfeedproject.domain.comment.repository.CommentRepository;
 import com.sparta.newsfeedproject.domain.member.entity.Member;
 import com.sparta.newsfeedproject.domain.post.dto.PostRequestDto;
@@ -12,9 +11,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor

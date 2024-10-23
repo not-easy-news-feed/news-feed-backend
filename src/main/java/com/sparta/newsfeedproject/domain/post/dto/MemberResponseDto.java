fe78b12df53cb5e1839dc03a91370d9e57662e6a
coldrice99 <coldrice99@gmail.com>
2024-10-23 10:24:50 +0900

    feat: 불필요한 코드 제거
    - 중복되거나 사용되지 않는 코드 세그먼트를 정리하여 코드베이스를 정돈했습니다.
    - 프로젝트의 가독성과 유지보수성을 향상시켰습니다.
    
    Refs: #22

--- src/main/java/com/sparta/newsfeedproject/domain/post/dto/MemberResponseDto.java
+++ src/main/java/com/sparta/newsfeedproject/domain/post/dto/MemberResponseDto.java
@@ -3,9 +3,6 @@ package com.sparta.newsfeedproject.domain.post.dto;
 import com.sparta.newsfeedproject.domain.member.entity.Member;
 import lombok.Getter;
 
-/**
- * MemberResponseDto를 사용하면 Member 객체의 필요한 정보만을 포함하여 응답할 수 있습니다. 이를 통해 불필요한 데이터(예: friendsList, blockedList)의 lazy-loading 문제를 피할 수 있습니다.
- */
 @Getter
 public class MemberResponseDto {
     private Long id;
--- src/main/java/com/sparta/newsfeedproject/domain/post/dto/PostRequestDto.java
+++ src/main/java/com/sparta/newsfeedproject/domain/post/dto/PostRequestDto.java
@@ -1,10 +1,8 @@
 package com.sparta.newsfeedproject.domain.post.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import org.aspectj.weaver.ast.Not;
 
 @Getter
 public class PostRequestDto {

2d7531ad5a8d2f7a8d5cadcdb49d12a5a1c1f447
Younghan Yonn <dudgks1172@naver.com>
2024-10-23 14:53:34 +0900

    feat: 프로필 수정 기능 추가
    - 현재 비밀번호를 입력하여 올바른 경우에만 수정 가능
    - 예외처리
    * 본인 확인을 위해 입력한 현재 비밀번호가 일치하지 않은 경우
    * 현재 비밀번호와 동일한 비밀번호로 수정하는 경우
    
    Refs: #18

--- src/main/java/com/sparta/newsfeedproject/domain/member/controller/MemberController.java
+++ src/main/java/com/sparta/newsfeedproject/domain/member/controller/MemberController.java
@@ -1,21 +1,15 @@
 package com.sparta.newsfeedproject.domain.member.controller;
 
-import com.sparta.newsfeedproject.domain.jwt.JwtUtil;
-import com.sparta.newsfeedproject.domain.member.dto.LoginRequestDto;
 import com.sparta.newsfeedproject.domain.member.dto.MemberResponseDto;
-import com.sparta.newsfeedproject.domain.member.dto.SignupRequestDto;
 import com.sparta.newsfeedproject.domain.member.dto.UpdateRequestDto;
 import com.sparta.newsfeedproject.domain.member.entity.Member;
 import com.sparta.newsfeedproject.domain.member.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 @Slf4j
 @RestController
 @RequestMapping("/api/members")
@@ -23,35 +17,7 @@ import java.util.Optional;
 public class MemberController {
 
     private final MemberService memberService;
-    private JwtUtil jwtUtil;
 
-
-    //회원가입
-    @PostMapping("/signup")
-    public ResponseEntity<String> signup(@RequestBody SignupRequestDto requestDto) {
-        memberService.signup(requestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body("회원가입 완료");
-    }
-
-    //로그인
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequestDto requestDto, HttpServletResponse response) {
-        memberService.login(requestDto, response);
-        return ResponseEntity.ok("로그인 성공");
-    }
-
-    // 프로필 조회
-    @GetMapping("/{memberId}")
-    public ResponseEntity<?> getMember(@PathVariable Long memberId) {
-        Optional<Member> member = memberService.getMemberWithPosts(memberId);
-
-        // 프로필이 존재하지 않는 경우 404
-        if (member.isEmpty()) {
-            return new ResponseEntity<>("Member not found", HttpStatus.NOT_FOUND);
-        }
-
-        return new ResponseEntity<>(member.get(), HttpStatus.OK);
-    }
 
     // 프로필 수정
     @PutMapping("/{memberId}")

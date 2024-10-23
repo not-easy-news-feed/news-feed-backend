43971149a623c1fb03e405a9b011cf6f89a09279
Minwoo <alsdn2606@gmail.com>
2024-10-23 17:25:42 +0900

    refactor: 리베이스 및 리팩토링
    - 병합 완료
    
    Refs: #21

--- src/main/java/com/sparta/newsfeedproject/filter/AuthFilter.java
+++ src/main/java/com/sparta/newsfeedproject/filter/AuthFilter.java
@@ -15,7 +15,6 @@ import java.io.IOException;
 
 @Slf4j(topic = "AuthFilter")
 @Component
-@Order(2)
 public class AuthFilter implements Filter {
 
     private final MemberRepository memberRepository;
@@ -38,19 +37,16 @@ public class AuthFilter implements Filter {
             // 회원가입, 로그인 관련 API 는 인증 필요없이 요청 진행
             chain.doFilter(request, response); // 다음 Filter 로 이동
         } else {
-            // 나머지 API 요청은 인증 처리 진행
-            // 토큰 확인
+            // 나머지 API 요청은 인증 처리 진행 토큰 확인
             String tokenValue = jwtUtil.getTokenFromRequest(httpServletRequest);
 
             if (StringUtils.hasText(tokenValue)) { // 토큰이 존재하면 검증 시작
                 // JWT 토큰 substring
                 String token = jwtUtil.substringToken(tokenValue);
-
                 // 토큰 검증
                 if (!jwtUtil.validateToken(token)) {
                     throw new IllegalArgumentException("Token Error");
                 }
-
                 // 토큰에서 사용자 정보 가져오기
                 Claims info = jwtUtil.getUserInfoFromToken(token);
 
@@ -65,5 +61,4 @@ public class AuthFilter implements Filter {
             }
         }
     }
-
 }

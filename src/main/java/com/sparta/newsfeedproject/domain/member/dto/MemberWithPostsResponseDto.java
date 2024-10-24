c4ea1986b049275d1e60443557b168045ed2b7a5
inzulmi <inzulmi132@gmail.com>
2024-10-24 10:22:47 +0900

    Feat: post도 함께 조회

--- src/main/java/com/sparta/newsfeedproject/domain/member/dto/MemberWithPostsResponseDto.java
+++ src/main/java/com/sparta/newsfeedproject/domain/member/dto/MemberWithPostsResponseDto.java
@@ -1,16 +1,18 @@
 package com.sparta.newsfeedproject.domain.member.dto;
 
 import com.sparta.newsfeedproject.domain.member.entity.Member;
+import com.sparta.newsfeedproject.domain.post.dto.PostResponseDto;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 public class MemberWithPostsResponseDto {
     private final Long id;
     private final String email;
     private final String name;
-    // private final List<PostResponseDto> postResponseDtoList;
+     private final List<PostResponseDto> postResponseDtoList;
     private final LocalDateTime createdAt;
     private final LocalDateTime updatedAt;
 
@@ -18,7 +20,7 @@ public class MemberWithPostsResponseDto {
         this.id = member.getId();
         this.email = member.getEmail();
         this.name = member.getName();
-        // this.postResponseDtoList = member.getPosts().stream().map(PostResponseDto::new).toList();
+        this.postResponseDtoList = member.getPosts().stream().map(PostResponseDto::new).toList();
         this.createdAt = member.getCreatedAt();
         this.updatedAt = member.getUpdatedAt();
     }
